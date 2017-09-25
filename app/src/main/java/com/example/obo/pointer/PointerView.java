2d8b96d359fdcbe14dd2b25d0b2ea59d2ae4499e
obo <obo.lin@dingtone.me>
2017-09-26 01:26:56 +0800

    完成指针动画

--- app/src/main/java/com/example/obo/pointer/PointerView.java
+++ app/src/main/java/com/example/obo/pointer/PointerView.java
@@ -2,9 +2,14 @@ package com.example.obo.pointer;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.SweepGradient;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -15,15 +20,23 @@ import android.view.View;
 
 public class PointerView extends View {
 
-    Paint mWordPaint = new Paint();
-    Paint mPointPaint = new Paint();
+    private static final String TAG = "PointerView";
 
-    int mViewWidth;
-    int mViewHeight;
+    private static final int FONT_SIZE = 30;
 
-    int mDegreen = 15;
-    Path path ;
-    PointF wordPosition= new PointF();
+    private Paint mWordPaint = new Paint();
+    private Paint mPointPaint = new Paint();
+    private Paint mArchPaint = new Paint();
+    private RectF mArchRect;
+
+    private int mViewWidth;
+    private int mViewHeight;
+
+    private float mDegreen = 0;
+    private int mValue = 15;
+    private Path path ;
+    private PointF wordPosition = new PointF();
+    private PointF mPointCenter = new PointF();
 
     public PointerView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -33,41 +46,44 @@ public class PointerView extends View {
     }
 
     private void initPaint() {
-
         mPointPaint.setAntiAlias(true);
-
         mWordPaint.setAntiAlias(true);
-        mWordPaint.setTextSize(15);
-
+        mWordPaint.setTextSize(FONT_SIZE);
+        mArchPaint.setAntiAlias(true);
     }
 
     private void initPath() {
-        double degreen = mDegreen / 120f * 2 * Math.PI;
+        double degreen = - mDegreen * 2 * Math.PI + Math.PI ;
         float sinValue = (float) Math.sin(degreen);
         float cosValue = (float) Math.cos(degreen);
 
-        int pointLength = (mViewWidth - 90) >> 1;
-        float pointX =  pointLength * (1 + sinValue);
-        float pointY =  pointLength * (1 + cosValue);
+        int pointLength = (mViewWidth >> 1) - 60;
+        float pointX =  mPointCenter.x + pointLength * sinValue;
+        float pointY =  mPointCenter.y + pointLength * cosValue;
 
         path = new Path();
-        path.moveTo(pointLength + 2 * cosValue, pointLength - 2 * sinValue);
-        path.lineTo(pointLength - 2 * cosValue, pointLength + 2 * sinValue);
+        path.moveTo(mPointCenter.x + 2 * cosValue, mPointCenter.y  - 2 * sinValue);
+        path.lineTo(mPointCenter.x - 2 * cosValue, mPointCenter.y  + 2 * sinValue);
         path.lineTo(pointX , pointY);
     }
 
     private void initWordPosition() {
-
-        double degreen = mDegreen / 120f * 2 * Math.PI;
+        double degreen = - mDegreen * 2 * Math.PI + Math.PI;
         float sinValue = (float) Math.sin(degreen);
         float cosValue = (float) Math.cos(degreen);
 
-        int pointLength = (mViewWidth >> 1) - 40;
-        float pointX =  pointLength * (1 + sinValue);
-        float pointY =  pointLength * (1 + cosValue);
+        int pointLength = (mViewWidth >> 1) - 20;
+        float pointX =  mPointCenter.x + pointLength * sinValue;
+        float pointY =  mPointCenter.y + pointLength * cosValue;
 
-        wordPosition.x = pointX;
-        wordPosition.y = pointY;
+        String text = mValue + "\"";
+        Rect rect = new Rect();
+        mWordPaint.getTextBounds(text, 0, text.length(), rect);
+        int width = rect.width();//文本的宽度
+        int height = rect.height();//文本的高度
+
+        wordPosition.x = pointX - width / 2;
+        wordPosition.y = pointY + height / 2;
     }
 
     @Override
@@ -75,23 +91,40 @@ public class PointerView extends View {
         super.onLayout(changed, left, top, right, bottom);
         mViewWidth = getMeasuredWidth();
         mViewHeight = getMeasuredHeight();
+        mPointCenter = new PointF(mViewWidth / 2, mViewHeight / 2);
         initPath();
         initWordPosition();
+        mArchRect = new RectF(30, 30, mViewWidth - 60, mViewHeight - 60);
     }
 
-    public void setDegreen(int degreen) {
-        this.mDegreen = degreen;
+    public void setDegreen(int value) {
+        this.mValue = value;
+        this.mDegreen = value / 120f;
         initPath();
         initWordPosition();
         postInvalidate();
+
     }
+
+    int []mMinColors = {Color.TRANSPARENT, 0x2263A17E, Color.TRANSPARENT};
+    float []degreens = {0,  0.3f, 0.3f};
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawPath(path, mPointPaint);
-        canvas.drawText("" + mDegreen , wordPosition.x, wordPosition.y , mWordPaint);
-//        canvas.drawRect(0,0, 100, 100, mPointPaint);
+        canvas.drawText("" + mValue + "\"" , wordPosition.x, wordPosition.y , mWordPaint);
+//        canvas.drawText("" + mValue, 0, 10, mWordPaint);
+        degreens[2] = degreens[1] = mDegreen;
+        SweepGradient sweepGradient = new SweepGradient(canvas.getWidth() / 2,
+                canvas.getHeight() / 2, //以圆弧中心作为扫描渲染的中心以便实现需要的效果
+                mMinColors, //这是我定义好的颜色数组，包含2个颜色：#35C3D7、#2894DD
+                degreens);
+        Matrix matrix = new Matrix();
+        matrix.setRotate(- 90, mViewWidth / 2, mViewHeight / 2);
+        sweepGradient.setLocalMatrix(matrix);
+        mArchPaint.setShader(sweepGradient);
+        canvas.drawArc(mArchRect, 0, 360, true, mArchPaint);
     }
 
 }
