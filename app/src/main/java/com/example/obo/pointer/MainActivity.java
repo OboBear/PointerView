e6d77e681a301cff26af7f57aaf1aee9fea642cc
obo <obo.lin@dingtone.me>
2017-09-25 20:21:30 +0800

    添加指针动画

--- app/src/main/java/com/example/obo/pointer/MainActivity.java
+++ app/src/main/java/com/example/obo/pointer/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.obo.pointer;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    PointerView mPointerView;
+    Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPointerView = (PointerView) findViewById(R.id.point_view);
+
+        new Thread() {
+
+            int degreen = 15;
+
+            @Override
+            public void run() {
+                super.run();
+
+                while (true) {
+
+                    degreen += 2;
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mPointerView.setDegreen(degreen % 120);
+                        }
+                    });
+                    try {
+                        sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
     }
 }
--- app/src/main/java/com/example/obo/pointer/PointerView.java
+++ app/src/main/java/com/example/obo/pointer/PointerView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PointF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -14,30 +15,83 @@ import android.view.View;
 
 public class PointerView extends View {
 
-    Paint mPaint;
-    Paint mPointPaint;
+    Paint mWordPaint = new Paint();
+    Paint mPointPaint = new Paint();
+
+    int mViewWidth;
+    int mViewHeight;
 
     int mDegreen = 15;
-    Path path = new Path();
+    Path path ;
+    PointF wordPosition= new PointF();
 
     public PointerView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-
+        initPaint();
+        initPath();
+        initWordPosition();
     }
 
     private void initPaint() {
-        mPointPaint = new Paint();
-        path.moveTo();
+
+        mPointPaint.setAntiAlias(true);
+
+        mWordPaint.setAntiAlias(true);
+        mWordPaint.setTextSize(15);
+
     }
+
+    private void initPath() {
+        double degreen = mDegreen / 120f * 2 * Math.PI;
+        float sinValue = (float) Math.sin(degreen);
+        float cosValue = (float) Math.cos(degreen);
+
+        int pointLength = (mViewWidth - 90) >> 1;
+        float pointX =  pointLength * (1 + sinValue);
+        float pointY =  pointLength * (1 + cosValue);
+
+        path = new Path();
+        path.moveTo(pointLength + 2 * cosValue, pointLength - 2 * sinValue);
+        path.lineTo(pointLength - 2 * cosValue, pointLength + 2 * sinValue);
+        path.lineTo(pointX , pointY);
+    }
+
+    private void initWordPosition() {
+
+        double degreen = mDegreen / 120f * 2 * Math.PI;
+        float sinValue = (float) Math.sin(degreen);
+        float cosValue = (float) Math.cos(degreen);
+
+        int pointLength = (mViewWidth >> 1) - 40;
+        float pointX =  pointLength * (1 + sinValue);
+        float pointY =  pointLength * (1 + cosValue);
+
+        wordPosition.x = pointX;
+        wordPosition.y = pointY;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        mViewWidth = getMeasuredWidth();
+        mViewHeight = getMeasuredHeight();
+        initPath();
+        initWordPosition();
+    }
+
     public void setDegreen(int degreen) {
         this.mDegreen = degreen;
+        initPath();
+        initWordPosition();
+        postInvalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawPath();
-        canvas.drawRect(0,0, 100, 100, mPointPaint);
+        canvas.drawPath(path, mPointPaint);
+        canvas.drawText("" + mDegreen , wordPosition.x, wordPosition.y , mWordPaint);
+//        canvas.drawRect(0,0, 100, 100, mPointPaint);
     }
 
 }
